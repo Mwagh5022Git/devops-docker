dc6fe00b6d0548f759bacdafdbdde9146030a292
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-02-24 11:04:06 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,19 +3,14 @@
 # Step 1: Install GitHub CLI (gh)
 sudo apt update
 #sudo apt install gh -y
-
-
 # Check install:
-
 gh --version
 
 # Step 2: Login to GitHub from terminal
 gh auth login
 git config --global user.email "mwagh5022@gmail.com"
 git config --global user.name "Manish Wagh"
-
 # Verify:
-
 gh auth status
 
 # Step 3: Go to your project folder
@@ -23,35 +18,28 @@ cd ~/devops-linux
 
 # Step 4: Create new GitHub repository
 gh repo create
-
-# You’ll see prompts like:
-<< EOF
-Repository name: devops-linux
-Description: DevOps practice repo
-Visibility: Public / Private
-Add remote? Yes
-Push local commits? Yes
-
+    # You’ll see prompts like:
+    << EOF
+      Repository name: devops-linux
+      Description: DevOps practice repo
+      Visibility: Public / Private
+      Add remote? Yes
+      Push local commits? Yes
 EOF
 # 🎉 Repository is created on GitHub + connected to Ubuntu
 
 # 🔹 Step 5: Push code (if not auto-pushed)
 git push -u origin main
-
-
-# (or your branch)
-
+  # (or your branch)
 git push -u origin project_1
 
 # ✅ METHOD 2: Create Repo on GitHub Website + Connect from Ubuntu
 # 🔹 Step 1: Create repo on GitHub (browser)
 << EOF
-GitHub → New Repository
-
-Repo name
-
-❌ Do NOT add README
-Create Repository
+    GitHub → New Repository
+    Repo name
+    ❌ Do NOT add README
+    Create Repository
 EOF
 
 # 🔹 Step 2: Connect Ubuntu project to GitHub
